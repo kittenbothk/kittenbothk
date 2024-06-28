98aaf70acda91d221dc285d6cfc4882511a5c790
kittenbothk <kittenbotdummy@gmail.com>
2024-06-28 11:25:55 +0800

    jjj

--- conf.py
+++ conf.py
@@ -276,7 +276,7 @@ html_theme_options = {
 
 # AutoStructify
 def setup(app):
-    app.add_source_suffix('.md', 'markdown')
+    #app.add_source_suffix('.md', 'markdown')
     app.add_config_value('recommonmark_config', {
             'url_resolver': lambda url: github_doc_root + url,
             'auto_toc_tree_section': 'Contents',
